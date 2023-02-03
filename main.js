ba252e9852c035cac4e4668f14d57ff44d6b49a4
Christina <stinacon@gmail.com>
2023-02-02 20:47:11 -0500

    created and linked css, html, js; created const for entire directory div and opened for-of loop is JS

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("linked");
+
+const directory = document.querySelector('#directory');
+//creating variable to capture the large div containing the entire customer directory
+
+//as established in customers.js file, the array(list) of customers is called, "customers"
+for (let customer of customers) {
+
+}
